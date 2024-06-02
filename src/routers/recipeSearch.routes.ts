91d9e7c82a7a21c4f841367f96ab0b30ff55987d
Ivan <ivitacoronte@hotmail.com>
2024-06-02 20:20:53 +0100

    planner route

--- src/routers/recipeSearch.routes.ts
+++ src/routers/recipeSearch.routes.ts
@@ -1,5 +1,7 @@
 import * as express from "express";
-import { Recipe } from "../models/recipe.js";
+import { Recipe, RecipeDocumentInterface } from "../models/recipe.js";
+import { Allergen } from "../models/enum/allergen.js";
+import { FoodType } from "../models/enum/FoodType.js";
 
 export const recipeSearch = express.Router();
 
@@ -39,3 +41,62 @@ recipeSearch.post('/recipeSearchPerIngredients', async (req, res) => {
     res.status(500).json({ error: 'Error al procesar la solicitud' });
   }
 });
+
+
+recipeSearch.post('/planner', async (req, res) => {
+  const { excludeIngredients, excludeAllergens, days } = req.body;
+
+  if (!days) {
+    return res.status(400).send('Faltan parametros.');
+  }
+
+  try {
+    const plan = await generateMealPlan(excludeIngredients, excludeAllergens, days);
+    res.json(plan);
+  } catch (error) {
+    console.error('Error generando plan:', error);
+    res.status(500).send('Internal Server Error');
+  }
+  return
+});
+
+const filterRecipes = async (excludeIngredients: string[], excludeAllergens: Allergen[]) => {
+  return await Recipe.find({
+    'ingredients.ingredientID': { $nin: excludeIngredients },
+    allergens: { $nin: excludeAllergens }
+  });
+};
+
+const generateMealPlan = async (excludeIngredients: string[], excludeAllergens: Allergen[], days: number) => {
+  const filteredRecipes: RecipeDocumentInterface[] = await filterRecipes(excludeIngredients, excludeAllergens);
+
+  const starters = filteredRecipes.filter(recipe => recipe.foodType === FoodType.Entrante);
+  const mainCourses = filteredRecipes.filter(recipe => recipe.foodType === FoodType.PlatoPrincipal);
+  const desserts = filteredRecipes.filter(recipe => recipe.foodType === FoodType.Postre);
+
+  const getRandomRecipe = (recipes: RecipeDocumentInterface[], exclude: RecipeDocumentInterface[]) => {
+    let availableRecipes = recipes.filter(recipe => !exclude.includes(recipe._id.toString()));
+    if (availableRecipes.length === 0) {
+      availableRecipes = recipes;
+    }
+    const randomIndex = Math.floor(Math.random() * availableRecipes.length);
+    return availableRecipes[randomIndex];
+  };
+
+  const plan = [];
+  const usedRecipes: { starter: RecipeDocumentInterface[], mainCourse: RecipeDocumentInterface[], dessert: RecipeDocumentInterface[] } = { starter: [], mainCourse: [], dessert: [] };
+
+  for (let i = 0; i < days; i++) {
+    const starter = getRandomRecipe(starters, usedRecipes.starter);
+    const mainCourse = getRandomRecipe(mainCourses, usedRecipes.mainCourse);
+    const dessert = getRandomRecipe(desserts, usedRecipes.dessert);
+
+    usedRecipes.starter.push(starter._id.toString());
+    usedRecipes.mainCourse.push(mainCourse._id.toString());
+    usedRecipes.dessert.push(dessert._id.toString());
+
+    plan.push({ day: i + 1, starter, mainCourse, dessert });
+  }
+
+  return plan;
+};
